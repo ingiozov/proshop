aa8868eadf56db5c73a911ee46a788a9fb9700b1
ingiozov <ingiozov@noreply.github.com>
2021-05-29 12:43:48 +0300

    rating added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, Nav, Container, Link } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
   return (
